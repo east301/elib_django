e37b2ee5cf9e66a15b3c03cdef3d87789e09e2d4
east301 <me@east301.net>
2019-01-20 10:07:01 +0900

    add reregister_model_admin

--- elib/django/admin.py
+++ elib/django/admin.py
@@ -5,6 +5,17 @@
 # https://opensource.org/licenses/Apache-2.0
 #
 
+from django.contrib import admin
+
+
+def reregister_model_admin(model):
+    def apply(model_admin):
+        admin.site.unregister(model)
+        admin.site.register(model, model_admin)
+        return model_admin
+
+    return apply
+
 
 def short_description(description):
     def apply(func):
