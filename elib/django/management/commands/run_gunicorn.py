a48e2208c9f3ec5dfaad86eec58309a08149b1ba
east301 <me@east301.net>
2019-01-20 16:43:35 +0900

    add --script-name option

--- elib/django/management/commands/run_gunicorn.py
+++ elib/django/management/commands/run_gunicorn.py
@@ -5,6 +5,8 @@
 # https://opensource.org/licenses/Apache-2.0
 #
 
+import os
+
 from django.core.management import BaseCommand
 from gunicorn.app.base import BaseApplication
 
@@ -15,8 +17,14 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument('-b', '--bind', default='127.0.0.1:8000')
         parser.add_argument('-w', '--workers', type=int, default=1)
+        parser.add_argument('-s', '--script-name')
 
     def handle(self, *args, **kwargs):
+        #
+        if kwargs.get('script_name'):
+            os.environ['SCRIPT_NAME'] = args.script_name
+
+        #
         DjangoApplication(kwargs['bind'], kwargs['workers']).run()
 
 
